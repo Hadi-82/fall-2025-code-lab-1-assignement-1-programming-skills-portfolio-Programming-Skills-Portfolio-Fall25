3387a04bc750b1fdc437edfa3e166f6ac24d946a
Hadi-82 <shadinzh82@gmail.com>
2025-11-21 11:25:58 +0400

    Create Exercise 02 Code.py
    Exercise 02 Solution Code

---
+++ Exercise 02- Simple Sums/Exercise 02 Code.py
@@ -0,0 +1,4 @@
+x = 8                   # Declaring a variable x and initializing it with the integer value `8`
+y= 10                   # Declaring a second variable y and initializing it with the integer value `10`
+sum = x + y             # Declaring a third variable sum that stores the sum of first two numbers
+print(sum)              # Printing the value of the sum to the console
