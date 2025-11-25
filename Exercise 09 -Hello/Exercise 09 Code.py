5535d01d883556b56d5e48759462d5f0504f2d16
Hadi-82 <shadinzh82@gmail.com>
2025-11-25 19:56:19 +0400

    Create Exercise 09 Code.py
    Exercise 09 Solution Code is to fill in the blanks in the code so that the function hello() prints "Hello" to the console.

---
+++ Exercise 09 -Hello/Exercise 09 Code.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello")                  # To print "Hello" to the console
+
+def main():
+    hello()                         # To call the hello() function
+
+if __name__ == "__main__":
+    main()
