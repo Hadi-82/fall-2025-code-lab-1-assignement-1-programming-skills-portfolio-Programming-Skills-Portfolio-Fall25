213d4df1497a108ead75cb370ef5297301f78535
Hadi-82 <shadinzh82@gmail.com>
2025-11-25 19:41:37 +0400

    Create Exercise 07 Code
    Exercise 07 Solution Code to create a program that prints the given values using loop.

---
+++ Exercise 07 - Some Counting/Exercise 07 Code.py
@@ -0,0 +1,19 @@
+print("Loop 1 : ")                                                           # a loop that counts up from 0 to 50 in increments of 1
+for i in range(51):
+    print(i)
+
+print("Loop 2 : ")                                                           # a loop that counts down from 50 to 0 in decrements of 1
+for i in range(50,-1,-1):
+    print(i)
+
+print("Loop 3 : ")                                                           # a loop that counts up from 30 to 50 in increments of 1
+for i in range (30,51):
+    print(i)
+
+print("Loop 4 : ")                                                           # a loop that counts down from 50 to 10 in decrements of 2
+for i in range (50,8,-2):
+    print(i)
+
+print("Loop 5 : ")                                                           # a loop that counts up from 100 to 200 in increments of 5
+for i in range(100,205,5):
+    print(i)
